1960dd61aad0c5981cedc9390cf07917d8d85257
ElenaSolovyeva <elena.solo.mail2014@gmail.com>
2024-03-20 13:53:57 +0100

    Task 2: add plugin application

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -3,6 +3,6 @@ package hexlet.code;
 public class App {
     public static void main(String[] args) {
 
-        System.out.printf("Hello and welcome!");
+        System.out.printf("Welcome to the Brain Games!");
     }
 }
